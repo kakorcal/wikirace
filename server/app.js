48622ac870653aa2d4a80dd1a753a2a0c7df5ed3
Kenneth Korcal <kakorcal@gmail.com>
2016-07-05 15:56:39 -0700

    adding boilerplate middleware

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes/index');
+const game = require("./game_server");
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+if(app.get('env') === 'development'){
+  require('dotenv').load();
+}
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/javascripts', express.static(`${__dirname}../client/javascripts`));
+app.use('/stylesheets', express.static(`${__dirname}../client/stylesheets`));
+app.use('/assets', express.static(`${__dirname}../client/assets`));
+app.use('/views', express.static(`${__dirname}../client/views`));
+app.use('/api', routes.users);
+
+app.get('*', (req, res)=>{
+  res.sendFile('views/layout.html', {root: './client'});
+});
+
+server.listen(PORT, ()=>{
+  console.log(`Listening to port ${PORT}`);
+});
+
+io.on('connection', (socket)=>{
+  game.init(io, socket);
+})
