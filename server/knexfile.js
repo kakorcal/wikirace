16d7be36fb64075c1068bd03194c328946b145fd
Kenneth Korcal <kakorcal@gmail.com>
2016-07-06 14:30:08 -0700

    setting up production environment

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,5 +5,21 @@ module.exports = {
     connection: {database: 'wikirace'},
     migrations: {directory: './db/migrations'},
     seeds: {directory: './db/seeds'}
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'      
+    }
   }
 };
