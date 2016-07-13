48445cd460ef3b967849308f74d999f96d8009c9
Kenneth Korcal <kakorcal@gmail.com>
2016-07-12 19:02:37 -0700

    can toggle time

--- client/javascripts/components/game/one_player.controller.js
+++ client/javascripts/components/game/one_player.controller.js
@@ -2,17 +2,33 @@
   angular.module('one_player.controller', ['ngSanitize'])
     .controller('OnePlayerGame', OnePlayerGame);
 
-  OnePlayerGame.$inject = ['Socket', '$location', '$ngBootbox'];
-  function OnePlayerGame(Socket, $location, $ngBootbox){
+  OnePlayerGame.$inject = ['$scope', 'Socket', '$location', '$ngBootbox'];
+  function OnePlayerGame($scope, Socket, $location, $ngBootbox){
     let vm = this;
     vm.clicks = 0;
+    vm.timerRunning = true;
     
+    vm.toggleSound = function(){
+      
+    };
+
+    vm.toggleTime = function(){
+      if(vm.timerRunning){
+        $scope.$broadcast('timer-stop');
+        vm.timerRunning = false;
+      }else{
+        $scope.$broadcast('timer-resume');
+        vm.timerRunning = true;
+      }
+    };
+
     vm.quitGame = function(){
       $ngBootbox.confirm('Are You Sure?').then(()=>{
         Socket.disconnect(true);
         $location.path('/play');
       });
     };
+
 
     Socket.connect().emit('Setup One Player Game');
 
