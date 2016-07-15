90072e845bdeafd2bf5b4fcdbd11cd52d6914839
Kenneth Korcal <kakorcal@gmail.com>
2016-07-15 15:35:49 -0700

    dotenv only for local

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,7 @@
 'use strict';
-require('dotenv').load();
+if ((process.env.NODE_ENV || 'development') === 'development') {
+  require('dotenv').load();
+} 
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
