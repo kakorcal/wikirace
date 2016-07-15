2936d43eb777c69aeb7aa66259ba4e00e92afadf
Kenneth Korcal <kakorcal@gmail.com>
2016-07-15 16:13:51 -0700

    update show page

--- client/javascripts/components/users/userShow.controller.js
+++ client/javascripts/components/users/userShow.controller.js
@@ -6,6 +6,7 @@
   function UserShowController({data:user}, UserService, $location){
     let vm = this;
     vm.user = user;
-    
+    console.log(user);
+    // name, image, 1p_score total, 2p_score total, total win, total lose, rank
   }
 })();
